756f7dc7e1583215131b02ff8681f6493e4ac4db
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2024-04-04 09:34:58 +0200

    a
    a

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 st.title('Scientific practice')
 st.text('Texto prueba prueba prueba')
-st.video('')
+st.video('/Users/alonso/Documents/projects-code/PracticeScience/Videos/IMG_6137.MOV')
 
