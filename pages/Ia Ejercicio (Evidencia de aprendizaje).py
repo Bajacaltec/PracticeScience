af12ff96c3d5a8705b93b841c8885cac37579a5a
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2024-04-24 16:22:12 +0200

    a
    a

--- pages/Ia Ejercicio (Evidencia de aprendizaje).py
+++ pages/Ia Ejercicio (Evidencia de aprendizaje).py
@@ -1,1 +1,2 @@
 import streamlit as st
+st.text('Completa los siguientes ejercicios, los datos serán enviados a tu correo electrónico y el de tu docente, conservalos como comprobante de aprendizaje')
