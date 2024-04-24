f277fcfd946974209c6c12a622689ee41febbe5e
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2024-04-24 12:19:00 +0200

    a

--- pages/Tema 1.py
+++ pages/Tema 1.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
+import streamlit as st
+from streamlit_timeline import timeline
+
+
+# use full page width
+st.set_page_config(page_title="Timeline Example", layout="wide")
+
+# load data
+with open('example.json', "r") as f:
+    data = f.read()
+
+# render timeline
+timeline(data, height=800)
 st.title('Historía de la investigación')
