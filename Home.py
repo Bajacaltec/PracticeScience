47dec8c14631041fd544ee6dd94848ab4ad7a56c
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2024-04-24 12:56:05 +0200

    a

--- Home.py
+++ Home.py
@@ -1,13 +1,15 @@
 import streamlit as st 
 import pandas as pd 
-from streamlit_lottie import st_lottie
+import psycopg2
+
+#from streamlit_lottie import st_lottie
 
 
-st_lottie("https://lottie.host/7821495d-4a43-4f88-8eaf-b7b3f1a97a6e/RRYQvDBKOw.json")
+#st_lottie("https://lottie.host/7821495d-4a43-4f88-8eaf-b7b3f1a97a6e/RRYQvDBKOw.json")
 
 
 st.title('Metodología de la investigación 1') 
-st.subheader('Docentes responsables')
+st.subheader('Docente responsable')
 
 col1, col2=st.columns(2)
 with col1:
@@ -22,3 +24,34 @@ st.error('Al inicio de la clase se realizará un miniexamen con preguntas aleato
 
 with st.expander('Datos en los que se basa la forma de trabajo'):
     st.text('En que se basa esta metodología')
+
+import psycopg2
+
+# Parámetros de conexión a la base de datos
+dbname = 'bajacaltec_ciencia'
+user = 'bajacaltec_ciencia_user'
+password = 'QnVGnpcQGxEr7q9W3YDiPS4ABxSTkAVn'
+host = 'dpg-cokdc7gl5elc73c3klp0-a.oregon-postgres.render.com'
+
+# Definir la sentencia SQL para crear la tabla
+create_table_query = '''
+CREATE TABLE db (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100),
+    edad INT
+);
+'''
+
+# Conexión a la base de datos y creación de la tabla
+try:
+    conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
+    cursor = conn.cursor()
+    cursor.execute(create_table_query)
+    conn.commit()
+    print("La tabla 'db' ha sido creada correctamente.")
+except psycopg2.Error as e:
+    print("Error al crear la tabla:", e)
+finally:
+    if conn:
+        cursor.close()
+        conn.close()
